0aeeec1f289ec14bffdf10d26fd6da05d96389d0
Gary Slopsema <gslopsema@gmail.com>
2013-07-05 18:47:12 -0400

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject ao-doll-smuggler "0.1.0"
+  :description "A program to streamline drug trafficking logistics"
+  :url "http://github.com/garslo/ao-doll-smuggler"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main ao-doll-smuggler.core)
