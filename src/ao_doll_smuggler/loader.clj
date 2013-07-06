228cf413ac1711e033486a423e631981bb4fdba8
Gary Slopsema <gslopsema@gmail.com>
2013-07-06 19:13:22 -0400

    Added function docstrings

--- src/ao_doll_smuggler/loader.clj
+++ src/ao_doll_smuggler/loader.clj
@@ -4,15 +4,21 @@
 
 
 (defn max-weight-line? [line]
+  "A predicate determining if `line' is formatted as a 'max weight'
+   line."
   (let [max-weight-regex #"^\s*max weight:\s*\d+\s*$"]
     (re-find max-weight-regex line)))
 
 (defn get-max-weight-from-line [line]
+  "Assuming `line' is a 'max weight' line (verified by
+   `max-weight-line?', extracts the indicated weight as an integer."
   (let [data-sections (split line #":")
         weight-string (trim (last data-sections))]
     (Integer/parseInt weight-string)))
 
 (defn get-max-weight [data]
+  "Given `data', a string with the contents of a suitable input file,
+   returns the maximum weight as an integer."
   (let [lines (split-lines data)
         weight-line (first (filter #'max-weight-line? lines))]
     (get-max-weight-from-line weight-line)))
