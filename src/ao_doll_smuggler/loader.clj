ffb85e1fd742775f302a53c376b270e1b1f648aa
Gary Slopsema <gslopsema@gmail.com>
2013-07-06 19:02:02 -0400

    Added initial get-max-weight test/impl

--- src/ao_doll_smuggler/loader.clj
+++ src/ao_doll_smuggler/loader.clj
@@ -2,8 +2,7 @@
   (:require [clojure.string :refer [split split-lines trim]])
   (:gen-class))
 
-(defn get-max-weight [data]
-  (let [data-sections (split data #":")
+
 (defn max-weight-line? [line]
   (let [max-weight-regex #"^\s*max weight:\s*\d+\s*$"]
     (re-find max-weight-regex line)))
@@ -12,3 +11,8 @@
   (let [data-sections (split line #":")
         weight-string (trim (last data-sections))]
     (Integer/parseInt weight-string)))
+
+(defn get-max-weight [data]
+  (let [lines (split-lines data)
+        weight-line (first (filter #'max-weight-line? lines))]
+    (get-max-weight-from-line weight-line)))
--- test/ao_doll_smuggler/loader_test.clj
+++ test/ao_doll_smuggler/loader_test.clj
@@ -2,6 +2,16 @@
   (:require [clojure.test :refer :all]
             [ao-doll-smuggler.loader :refer :all]))
 
+;
+;(deftest test-get-max-weight-multiple-line-input
+;  (let [max-weight 20
+;        input-string (str "max weight: " max-weight "\n"
+;                          "\n"
+;                          "Lines of Data\n"
+;                          "foo 100   43")
+;        result (get-max-weight input-string)]
+;    (is (= max-weight result))))
+;
 (deftest test-max-weight-line?
   (is (max-weight-line? "max weight: 40"))
   (is (max-weight-line? "max weight:      23"))
@@ -18,8 +28,12 @@
                        (get-max-weight-from-line weight-string)))))]
     (run-single-test 10)
     (run-single-test 5)))
+
+;;; TODO: Add failure tests
 (deftest test-get-max-weight-single-line-input
-  (let [max-weight 10
-        input-string (str "max weight: " max-weight)
-        result (get-max-weight input-string)]
-    (is (= max-weight result))))
+  (letfn [(run-single-test [weight]
+            (let [input-string (build-max-weight-string weight)
+                  result (get-max-weight input-string)]
+              (is (= weight result))))]
+    (run-single-test 10)
+    (run-single-test 20)))
