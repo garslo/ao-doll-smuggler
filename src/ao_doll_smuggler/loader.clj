413f60c19996c0ef94cadf0c379cc319f01b17cd
Gary Slopsema <gslopsema@gmail.com>
2013-07-06 19:00:11 -0400

    Added max-weight-line? test/impl

--- src/ao_doll_smuggler/loader.clj
+++ src/ao_doll_smuggler/loader.clj
@@ -4,5 +4,8 @@
 
 (defn get-max-weight [data]
   (let [data-sections (split data #":")
+(defn max-weight-line? [line]
+  (let [max-weight-regex #"^\s*max weight:\s*\d+\s*$"]
+    (re-find max-weight-regex line)))
         weight-string (trim (last data-sections))]
     (Integer/parseInt weight-string)))
--- test/ao_doll_smuggler/loader_test.clj
+++ test/ao_doll_smuggler/loader_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [ao-doll-smuggler.loader :refer :all]))
 
+(deftest test-max-weight-line?
+  (is (max-weight-line? "max weight: 40"))
+  (is (max-weight-line? "max weight:      23"))
+  (is (not (max-weight-line? "max weight 40")))
+  (is (not (max-weight-line? "foo bar: 100"))))
 (deftest test-get-max-weight-single-line-input
   (let [max-weight 10
         input-string (str "max weight: " max-weight)
