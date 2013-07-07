fec890ccc81fc798c1be605f8d2710bc77940ce4
Gary Slopsema <gslopsema@gmail.com>
2013-07-07 19:05:53 -0400

    Bulk commit of knapsack tests/impl

--- src/ao_doll_smuggler/knapsack.clj
+++ src/ao_doll_smuggler/knapsack.clj
@@ -17,17 +17,67 @@
   (or (invalid-position? position)
       (invalid-weight? weight)))
 
+(defn get-doll [doll-data position]
+  (let [index (position->index position)]
+    (nth doll-data index nil)))
+
+;;; Mutual recursion between the following two and `max-value'
+(declare max-with-doll)
+(declare max-without-doll)
+
+(defn raw-max-value [doll-data position current-weight]
+  (let [doll (get-doll doll-data position)
         doll-can-fit? (fn []
-                        (> max-weight doll-weight))]
-    ;; Start of 0/1 knapsack algorithm
-    (if (invalid-position-or-weight? position max-weight)
+                        (> current-weight (doll :weight)))]
+    (if (invalid-position-or-weight? position current-weight)
       0
       (if (doll-can-fit?)
-        (max (max-without-this-doll)
-             (+ (value position doll-data) (max-with-this-doll)))
-        (max-without-this-doll)))))
+        (max (max-without-doll doll-data
+                               position
+                               current-weight)
+             (+ (doll :value)
+                (max-with-doll doll-data
+                               position
+                               current-weight)))
+        (max-without-doll doll-data
+                          position
+                          current-weight)))))
+
+;;; For efficiency. This implicitly creates the table necessary for a
+;;; dynamic programming algorithm.
+(def max-value (memoize raw-max-value))
+
+(defn max-with-doll [doll-data position current-weight]
+  (let [doll (get-doll doll-data position)
+        new-position (- position 1)
+        new-weight (- current-weight (doll :weight))]
+      (max-value doll-data new-position new-weight)))
+
+(defn max-without-doll [doll-data position current-weight]
+  (let [new-position (- position 1)]
+    (max-value doll-data new-position current-weight)))
 
 (defn create-knapsack-solver [doll-data]
   (fn [position max-weight]
     (max-value doll-data position max-weight)))
 
+(defn doll-is-included? [doll-data position weight]
+  (not (= (max-value doll-data position weight)
+          (max-value doll-data (- position 1) weight))))
+
+(defn collect-dolls-in-solution [doll-data max-weight]
+  (letfn [(collect-dolls [position weight acc]
+            (let [doll (get-doll doll-data position)]
+                (cond
+                 (or (= weight 0) (= position 0)) acc
+                 (doll-is-included? doll-data
+                                    position
+                                    weight) (collect-dolls
+                                                      (dec position)
+                                                      (- weight
+                                                         (doll :weight))
+                                               (conj acc doll))
+                 true (collect-dolls (dec position)
+                                     weight
+                                     acc))))]
+    (collect-dolls (count doll-data) max-weight #{})))
--- test/ao_doll_smuggler/knapsack_test.clj
+++ test/ao_doll_smuggler/knapsack_test.clj
@@ -1,7 +1,9 @@
 (ns ao-doll-smuggler.knapsack-test
   (:require [clojure.test :refer :all]
             [ao-doll-smuggler.knapsack :refer [create-knapsack-solver
-                                               collect-included-dolls]]))
+                                               max-value
+                                               collect-dolls-in-solution
+                                               ]]))
 
 ;; Example data and results table taken from
 ;; http://www.cse.msu.edu/~torng/Classes/Archives/cse830.03fall/Lectures/Lecture10.ppt
@@ -9,28 +11,28 @@
                  {:name "B" :weight 3 :value 50}
                  {:name "C" :weight 1 :value 100}
                  {:name "D" :weight 5 :value 95}
-                 {:name "E" :weight 3 :value 30}]
-      max-value-fn (create-knapsack-solver doll-data)]
+                 {:name "E" :weight 3 :value 30}]]
 
   (deftest test-max-value-as-output-table
-   (let [result-empty-0 (max-value-fn 0 0)
-         result-A-0 (max-value-fn 1 0)
-         result-empty-10 (max-value-fn 0 10)
-         result-E-10 (max-value-fn 5 10)
-         result-A-1 (max-value-fn 1 1)]
+   (let [result-empty-0 (max-value doll-data 0 0)
+         result-A-0 (max-value doll-data 1 0)
+         result-empty-10 (max-value doll-data 0 10)
+         result-E-10 (max-value doll-data 5 10)
+         result-A-1 (max-value doll-data 1 1)]
      (is (= 0 result-empty-0))
      (is (= 0 result-A-0))
      (is (= 0 result-empty-10))
      (is (= 245 result-E-10))
-     (is (= 0 result-A-1))
-     ))
+     (is (= 0 result-A-1))))
 
- (deftest test-collect-included-dolls
-   (let [max-weight 10
-         included-dolls #{{:name "B" :weight 3 :value 50}
-                          {:name "C" :weight 1 :value 100}
-                          {:name "D" :weight 5 :value 95}}
-         result (collect-included-dolls #'max-value-fn
-                                        max-weight
-                                        doll-data)]
-     (is (= result included-dolls)))))
+;  (deftest test-doll-is-included
+;    (is (doll-is-included? doll-data 1)))
+
+  (deftest test-collect-included-dolls
+    (let [max-weight 10
+          included-dolls #{{:name "B" :weight 3 :value 50}
+                           {:name "C" :weight 1 :value 100}
+                           {:name "D" :weight 5 :value 95}}
+          result (collect-dolls-in-solution doll-data
+                                            max-weight)]
+    (is (= result included-dolls)))))
