3db38a2d3961d52024b6cca3139c0ee928d38ddc
Gary Slopsema <gslopsema@gmail.com>
2013-07-06 22:05:34 -0400

    Basic command line arg processing

--- src/ao_doll_smuggler/core.clj
+++ src/ao_doll_smuggler/core.clj
@@ -1,4 +1,5 @@
 (ns ao-doll-smuggler.core
+  (:require [clojure.tools.cli :refer [cli]])
   (:gen-class))
 
 (defn -main
@@ -6,4 +7,12 @@
   [& args]
   ;; work around dangerous default behaviour in Clojure
   (alter-var-root #'*read-eval* (constantly false))
-  (println "Hello, World!"))
+  (let [[options args banner]
+        (cli args
+             ["-i" "--input-file" "Doll input file"])]
+    (println "input file: " (options :input-file)))
+  )
+;  (with-command-line args
+;    "Stuff goes here"
+;    [[input-file "Doll input file to use"]]
+;    (println input-file)))
