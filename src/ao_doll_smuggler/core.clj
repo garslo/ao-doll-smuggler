580a52eb401f1d8c6d7c3b54bb833fe8843c3857
Gary Slopsema <gslopsema@gmail.com>
2013-07-07 19:56:35 -0400

    Front-end refactoring

--- src/ao_doll_smuggler/core.clj
+++ src/ao_doll_smuggler/core.clj
@@ -1,34 +1,47 @@
 (ns ao-doll-smuggler.core
-  (:require [clojure.tools.cli :refer [cli]]
+  (:require; [clojure.tools.cli :refer [cli]]
+            [clojure.java.io :refer [as-file]]
             [ao-doll-smuggler.loader :refer [get-doll-info
                                              get-max-weight]]
             [ao-doll-smuggler.knapsack :refer [collect-dolls-in-solution]])
   (:gen-class))
 
 (defn print-solution [dolls]
+  (println "packed dolls:")
+  (println)
+  (println (format "%s%10s%8s" "name" "weight" "value"))
   (doseq [doll dolls]
     (let [name (doll :name)
           weight (doll :weight)
           value (doll :value)]
-      (println name "\t" weight "\t" value))))
+      (println (format "%s\t%s\t%s" name weight value)))))
+
+(defn invalid-input? [weight doll-data]
+  (or (not weight)
+      (empty? doll-data)))
+
+(defn invalid-file? [filename]
+  (or (empty? filename)
+      (not (.exists (as-file filename)))))
+
+(defn die-if-invalid [args]
+  (if (or (empty? args)
+          (invalid-file? (first args)))
+    (do
+      (println "Please specify a valid input file.")
+        (System/exit 0))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Entry point to the doll smuggling knapsack problem."
   [& args]
-  ;; work around dangerous default behaviour in Clojure
-  (alter-var-root #'*read-eval* (constantly false))
-  (let [[options args banner] (cli args
-                                   ["-i" "--input-file" "Doll input file"])
-        input-file (first args)]
-
-    (if (not input-file)
-      (do
-        (println "Please specify an input file.")
-        (System/exit 0)))
-
-    (let [contents (slurp input-file)
-          max-weight (get-max-weight contents)
-          doll-data (get-doll-info contents)
-          dolls (collect-dolls-in-solution doll-data
-                                           max-weight)]
-      (print-solution dolls))))
+  (die-if-invalid args)
+  (let [input-file (first args)
+        contents (slurp input-file)
+        max-weight (get-max-weight contents)
+        doll-data (get-doll-info contents)]
+        (if (invalid-input? max-weight doll-data)
+          (do
+            (println "Malformed input file.")
+            (System/exit 0)))
+        (print-solution (collect-dolls-in-solution doll-data
+                                                   max-weight))))
