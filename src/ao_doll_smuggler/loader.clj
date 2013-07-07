6b5ceab8353d41d61128ba6e6b3c5eb344946caf
Gary Slopsema <gslopsema@gmail.com>
2013-07-07 19:56:04 -0400

    Code cleanup

--- src/ao_doll_smuggler/loader.clj
+++ src/ao_doll_smuggler/loader.clj
@@ -9,12 +9,14 @@
   (let [max-weight-regex #"^\s*max weight:\s*\d+\s*$"]
     (re-find max-weight-regex line)))
 
+
 (defn get-max-weight-from-line [line]
   "Assuming `line' is a 'max weight' line (verified by
    `max-weight-line?'), extracts the indicated weight as an integer."
   (let [data-sections (split line #":")
         weight-string (trim (last data-sections))]
-    (Integer/parseInt weight-string)))
+    (Integer/parseInt weight-string))))
+
 
 (defn get-max-weight [data]
   "Given `data', a string with the contents of a suitable input file,
@@ -23,12 +25,14 @@
         weight-line (first (filter #'max-weight-line? lines))]
     (get-max-weight-from-line weight-line)))
 
+
 (defn doll-info-line? [line]
   "A predicate determining if `line' is formatted as a doll
   information line."
   ;; A doll info line looks like "<alphabetic_name> <digits> <digits>"
   (let [doll-info-regex #"^\s*[a-zA-Z]+\s+\d+\s+\d+"]
    (re-find doll-info-regex line)))
+
 
 (defn get-doll-info-from-line [line]
   "Retrieves the doll information from `line'. Returns a
@@ -43,6 +47,7 @@
         weight (Integer/parseInt (nth doll-info 1))
         value (Integer/parseInt (nth doll-info 2))]
     (hash-map :name name :weight weight :value value)))
+
 
 (defn get-doll-info [data]
   "Given `data', a string with the contents of a suitable input file,
