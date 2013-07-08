8418f8877080d845c8b3f6d29eb76079d1f8b2c9
Gary Slopsema <gslopsema@gmail.com>
2013-07-07 20:11:17 -0400

    Added docstrings

--- src/ao_doll_smuggler/knapsack.clj
+++ src/ao_doll_smuggler/knapsack.clj
@@ -19,6 +19,7 @@
 
 
 (defn get-doll [doll-data position]
+  "Retrieves the doll stored in `doll-data' at position `position'."
   (let [index (position->index position)]
     (nth doll-data index nil)))
 
@@ -28,6 +29,13 @@
 (declare max-without-doll)
 
 (defn raw-max-value [doll-data position current-weight]
+  "The maximum value function for `doll-data' using the first
+   `position' dolls at `current-weight' max weight.
+
+   This function is the core of the 0/1-knapsack algorithm. Its
+   purpose is to provide values for a `position'x`max-weight'
+   table. Rather than use an explicit table, we memoize this
+   function below and use the memoized function as a table."
   (let [doll (get-doll doll-data position)
         doll-can-fit? (fn []
                         (> current-weight (doll :weight)))]
@@ -50,12 +58,16 @@
 (def max-value (memoize raw-max-value))
 
 (defn max-with-doll [doll-data position current-weight]
+  "Returns the maximum value obtainable with the doll at `position'
+   with max weight `current-weight' placed in the knapsack."
   (let [doll (get-doll doll-data position)
         new-position (- position 1)
         new-weight (- current-weight (doll :weight))]
       (max-value doll-data new-position new-weight)))
 
 (defn max-without-doll [doll-data position current-weight]
+  "Returns the maximum value obtainable without the doll at `position'
+   with max weight `current-weight' in the knapsack."
   (let [new-position (- position 1)]
     (max-value doll-data new-position current-weight)))
 
@@ -64,6 +76,8 @@
     (max-value doll-data position max-weight)))
 
 (defn doll-is-included? [doll-data position weight]
+  "Determines if the doll at `position' and `weight' is included in
+   the optimal knapsack solution."
   (not (= (max-value doll-data position weight)
           (max-value doll-data (- position 1) weight))))
 
